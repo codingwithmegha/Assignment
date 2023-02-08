be26ad51d384a2d5642c4bcdfef668a07a9273a2
codingwithmegha <122961155+codingwithmegha@users.noreply.github.com>
2023-02-08 17:55:28 +0530

    Add files via upload

---
+++ face_draw.java
@@ -0,0 +1,11 @@
+package Method_program;
+
+public class face_draw {
+public static void main(String[] args) {
+	System.out.println(" +\"\"\"\"\"+");
+	System.out.println(" [|o o|]");
+	System.out.println(" |  ^  |");
+	System.out.println(" | '-' |");
+	System.out.println(" +-----+");
+}
+}
