774f481a07269a3dd79dbecf8178f29cb3fce097
codingwithmegha <122961155+codingwithmegha@users.noreply.github.com>
2023-02-08 12:56:12 +0530

    Add files via upload

---
+++ Dowhile2.java
@@ -0,0 +1,15 @@
+package controlSystem;
+
+public class Dowhile2 {
+	public static void main(String[] args) {
+		
+	int i=1;
+	System.out.println("First 10 odd number");
+	do {
+		System.out.println(i);
+		i=i+2;
+	}while(i<=20);
+
+	}
+
+}
