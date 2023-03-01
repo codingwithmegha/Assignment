32e1cef37c3e3ce0168fce00fa1ccb53675f204c
codingwithmegha <122961155+codingwithmegha@users.noreply.github.com>
2023-03-01 18:27:47 +0530

    Add files via upload

---
+++ movie.java
@@ -0,0 +1,33 @@
+package mypackage;
+public class movie {
+	private String movieName;
+	private String movieStartTime;
+	private String movieEndTime;
+	private int moviePrice;
+	public String getMovieName() {
+		return movieName;
+	}
+	public void setMovieName(String movieName) {
+		this.movieName = movieName;
+	}
+	public String getMovieStartTime() {
+		return movieStartTime;
+	}
+	public void setMovieStartTime(String movieStartTime) {
+		this.movieStartTime = movieStartTime;
+	}
+	public String getMovieEndTime() {
+		return movieEndTime;
+	}
+	public void setMovieEndTime(String movieEndTime) {
+		this.movieEndTime = movieEndTime;
+	}
+	public int getMoviePrice() {
+		return moviePrice;
+	}
+	public void setMoviePrice(int moviePrice) {
+		this.moviePrice = moviePrice;
+	}
+
+}
+	
