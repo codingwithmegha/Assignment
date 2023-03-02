1ea3c1740b5a98306a7a2ff72d2be9d396746ce6
codingwithmegha <122961155+codingwithmegha@users.noreply.github.com>
2023-03-02 17:55:32 +0530

    Add files via upload

---
+++ Array_name.java
@@ -0,0 +1,9 @@
+package Array;
+public class Array_name {  //main class
+public static void main(String[] args) {  //main method
+	String [] name= {"Ram","Suman","Karan","Anuj","Avni","Riya","Sanjay","Bhumi","Disha","shivani"}; //declaring & assigning
+    for(int i=0; i<name.length; i++) {  //for loop for name
+    	System.out.println(name[i]);  //for printing the name
+    }
+}
+}
