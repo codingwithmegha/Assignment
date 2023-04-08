c8e75ad61b543a378535b1e9902568244178f6a0
codingwithmegha <122961155+codingwithmegha@users.noreply.github.com>
2023-04-08 22:00:34 +0530

    Add files via upload

--- Home_Assignment/Even_Odd_8.java
+++ Home_Assignment/Even_Odd_8.java
@@ -1,9 +1,9 @@
 package Home_Assignment;
-public class Even_Odd_8 {
+public class Even_Odd_8 {  //main class
 	public static void main(String[] args) {   //main method   
-		int a[]= {1,2,3,4,5,6,7,8,9,10};//declaration and initiali
+		int a[]= {1,2,3,4,5,6,7,8,9,10};//declaration and initializing array
 		System.out.println("Even number ");
-		for(int i=0;i<a.length;i++) {
+		for(int i=0;i<a.length;i++) {   
 		if (a[i]%2==0) { // checking the even number
 		System.out.println(a[i]);
 		}}
--- Home_Assignment/copy_array_3.java
+++ Home_Assignment/copy_array_3.java
@@ -1,7 +1,7 @@
 package Home_Assignment;
 //WAP to copy an array by iterating the array
 public class copy_array_3 {
-	public static void main(String[] args) {        
+	public static void main(String[] args) {   
 		//declaring a source array
 		char[] copyFrom= {'d','e','c','f','f','e','i','n'};
 		////declaring a Destination array
